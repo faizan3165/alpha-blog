b0e989725c1a75823a6f802a55ea16c089f06480
Faizan Mehmood <faizanmehmood3165@gmail.com>
2022-10-06 19:51:26 +0500

    add downcase method on email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  before_save {downcase_email}
+
   has_many :articlest
   
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25}
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # ! name constants by using uppercase letters
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX }
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
