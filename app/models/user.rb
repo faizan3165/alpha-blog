a54a4849c4c23e90a424e7f072cd55f9ca346841
Faizan Mehmood <faizanmehmood3165@gmail.com>
2022-10-06 19:28:58 +0500

    implement one-to-many association b/w user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25}
+  has_many :articles
+  
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # ! name constants by using uppercase letters
 
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105}, format: { with: VALID_EMAIL_REGEX }
 end
